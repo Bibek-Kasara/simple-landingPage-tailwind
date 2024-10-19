831a884c43bebdb0fbaca534d7c22f6d09147384
bibe1k <bibe1k>
2024-10-19 12:10:05 +0545

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"], // You can specify specific file types or folders here if needed.
+  theme: {
+    extend: {
+f
+    },
+  },
+  plugins: [],
+}
